4156584e4743948fbe1fcd87df6d67be27bb1d9f
eaguirrebena <eaguirrebena@alumnos.uai.cl>
2016-05-16 17:10:31 -0400

    version

--- Index.php
+++ Index.php
@@ -3,19 +3,19 @@ require_once (dirname ( __FILE__ ) . '/../../config.php');
 //require_once($CFG->dirroot.'/local/GIDTICS/forms.php');
 global $DB, $USER, $CFG, $OUTPUT;
 require_login ();
-$baseurl = new moodle_url ( '/local/GIDTICS/index.php' ); // clase pagina
+$baseurl = new moodle_url ( '/local/GID/index.php' ); // clase pagina
 $context = context_system::instance ();
 $PAGE->set_context ( $context );
 $PAGE->set_url ( $baseurl );
 $PAGE->set_pagelayout ( 'standard' );
-//$PAGE->set_title ( get_string ( 'title', 'local_GIDTICS' ) );
+//$PAGE->set_title ( get_string ( 'title', 'local_GID' ) );
 $PAGE->set_title ( 'Teachers Connection' );
-$PAGE->set_heading ( get_string ( 'title', 'local_GIDTICS' ) );
-$PAGE->navbar->add ( get_string ( 'title', 'local_GIDTICS' ) );
+$PAGE->set_heading ( get_string ( 'title', 'local_GID' ) );
+$PAGE->navbar->add ( get_string ( 'title', 'local_GID' ) );
 // $PAGE->navbar->add('index','reservar.php');
 echo $OUTPUT->header (); // shows header
                          // <link rel="stylesheet" href="css/style.css">
-echo $OUTPUT->heading ( get_string ( 'title', 'local_GIDTICS' ) );
+echo $OUTPUT->heading ( get_string ( 'title', 'local_GID' ) );
 echo $USER->firstname . " " . $USER->lastname . "  -  " . $USER->email;
 echo "<br>";
 // test
@@ -29,14 +29,14 @@ echo "<br>";
 $email = $USER->email;
 // $result = $DB->get_records_sql('SELECT * FROM {local_fitness} WHERE email = ?', array($email));
 // hasta aqui llega el header
-$sql = "SELECT * from {local_GIDTICS} order by Nombre asc";
+$sql = "SELECT * from {local_GID} order by Nombre asc";
 $resultado = $DB->get_records_sql ( $sql );
 $data = '';
 foreach ( $resultado as $resultados ) {
-	$editurl = new moodle_url ( '/local/GIDTICS/edit.php', array (
+	$editurl = new moodle_url ( '/local/GID/edit.php', array (
 			'id' => $resultados->id 
 	) );
-	$deleteurl = new moodle_url ( '/local/GIDTICS/delete.php', array (
+	$deleteurl = new moodle_url ( '/local/GID/delete.php', array (
 			'id' => $resultados->id 
 	) );
 	$data [] = array (
@@ -66,6 +66,6 @@ $table->head = array (
 $table->data = $data;
 // Render the table in a variable.
 echo html_writer::table ( $table );
-$inventariourl = new moodle_url ( '/local/GIDTICS/guardar.php');
+$inventariourl = new moodle_url ( '/local/GID/guardar.php');
 echo $OUTPUT->single_button ( $inventariourl, 'Agregar Inventario' );
 echo $OUTPUT->footer (); //shows footer 
--- version.php
+++ version.php
@@ -6,4 +6,4 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->pluginname = "local_gid";
 $plugin->component = 'local_gid';
-$plugin->version   = '2016051600';
+$plugin->version   = 2016051600;
