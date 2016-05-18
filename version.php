0104a9ca8960d058eacab9954dfd3b936f54218f
eaguirrebena <eaguirrebena@alumnos.uai.cl>
2016-05-18 12:11:45 -0400

    version fail

--- version.php
+++ version.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Version details
+*
+* @package    local
+* @subpackage notebookstore
+* @copyright  2016 Eduardo Aguirrebeña (eaguirrebena@alumnos.uai.cl)
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 //Pertenece al plugin Teachers Connection
  
- 
+
 defined('MOODLE_INTERNAL') || die();
-$plugin->pluginname = "local_gid";
-$plugin->component = 'local_gid';
+$plugin->pluginname = 'local_TeachersConnection';
+$plugin->component = 'local_TeachersConnection';
 $plugin->version   = 2016051600;
+
+/*
+defined('MOODLE_INTERNAL') || die();
+$plugin->component = 'local_TeachersConnection';
+$plugin->version = 2016051600;
+$plugin->release = 'v1.0';
+$plugin->requires = 2014051200;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->cron = 0;
+$plugin->dependencies = array();
+*/
